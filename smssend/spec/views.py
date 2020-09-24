683e20233813e7b75846a2cf374c142a29f7ec13
Agil89 <agil.makhmudov@mail.ru>
2020-09-24 15:54:53 +0400

    a

---
+++ smssend/spec/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import requests
+import json
+
+class PhoneNumber(APIView):
+    def get(self,request):
+        data = request.GET
+        driver = data.get('driver_id')
+        phone_number = data.get('phone_number')
+        headers = {
+            'api_key': '15b056aae8c5b037ff52ffa5b6cd1180',
+            'Content-type': 'application/json'
+        }
+        driver_request = requests.get(f'http://0727-dmitrov.ligataxi.com/api/v1/drivers/{driver}/',headers=headers)
+        driver_data=driver_request.json()
+        car = driver_data.get('car')
+        text = f'За вами приедет {car} '
+        after_url = {'number':phone_number,
+                     'text':text,
+                     'sign':'Tach Taxi',
+                     'channel':'DIRECT'}
+
+        url = requests.get('https://agil.makhmudov@mail.ru:Q9BqK8I8HXoEx1hVIhcE4gRkBVa1@gate.smsaero.ru/v2/sms/send?',params=after_url)
+        print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',url.status_code)
